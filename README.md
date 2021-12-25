ee2c90281c94d26beaf058056e22b1acaf46e9c3
Lex <gtagaev96@mail.ru>
2021-12-26 00:24:10 +0700

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Test app
+
+## О приложении
+В директории doc сгенерирована документация.
+
+Основано на паттерне проектирования BLoC с управлением состояниями.
+
+Содержит следующие модули:
+- Пользователи
+- Посты
+- Комментарии
+- Альбомы
+
+На основе SQFlite реализовано кэширование следующих данных:
+- Список пользователей
+
+## Использованы следующие пакеты
+
+| Название пакета |     Версия    |    Описание   |
+| --------------- | ------------- | ------------- |
+| [http][http]  | ^0.13.4  | Работа с протоколом HTTP |
+| [skeletons][skeletons]  | ^0.0.3  | Превью загрузки |
+| [cached_network_image][cached_network_image]  | ^3.2.0  | Виджет загрузки изображений |
+| [photo_view][photo_view]  | ^0.13.0  | Виджет просмотра изображений |
+| [sqflite][sqflite]  | ^2.0.1  | База данных |
+| [path_provider][path_provider]  | ^2.0.8  | Получение местоположений директорий |
+| [flutter_native_splash][flutter_native_splash]  | ^1.3.2  | Превью загрузки приложения |
+
+[http]: <https://pub.dev/packages/http>
+[skeletons]: <https://pub.dev/packages/skeletons>
+[cached_network_image]: <https://pub.dev/packages/cached_network_image>
+[photo_view]: <https://pub.dev/packages/photo_view>
+[sqflite]: <https://pub.dev/packages/sqflite>
+[path_provider]: <https://pub.dev/packages/path_provider>
+[flutter_native_splash]: <https://pub.dev/packages/flutter_native_splash>
+
---
+++ lib/modules/albums/albums_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'widgets/albums_card.dart';
+import 'widgets/image_gallery.dart';
+import 'bloc/albums_bloc.dart';
+import 'bloc/albums_state.dart';
+import 'model/albums_model.dart';
+
+/// Экран списка альбомов
+class AlbumsScreen extends StatefulWidget {
+  final int userId;
+
+  const AlbumsScreen({Key? key, required this.userId}) : super(key: key);
+
+  @override
+  _AlbumsScreenState createState() => _AlbumsScreenState();
+}
+
+class _AlbumsScreenState extends State<AlbumsScreen> {
+  late AlbumsBloc _bloc;
+  final List<AlbumsModel> _allAlbumsData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = AlbumsBloc();
+    _bloc.getAlbumsScreenBloc(userId: widget.userId, start: 0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _isTurn = MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return Scaffold(
+      body: StreamBuilder(
+        stream: _bloc.albumsScreenStreamController,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data is LoadedAlbumsState) {
+            final _data = snapshot.data as LoadedAlbumsState;
+            _allAlbumsData.addAll(_data.albumsData);
+
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: _isTurn ? 2 : 3,
+                mainAxisSpacing: 5.0,
+                crossAxisSpacing: 5.0,
+              ),
+              itemCount: _allAlbumsData.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == (_allAlbumsData.length - 2)) {
+                  _bloc.getAlbumsScreenBloc(
+                    userId: widget.userId,
+                    start: _allAlbumsData.length,
+                  );
+                }
+
+                return InkWell(
+                  splashColor: Colors.transparent,
+                  onTap: () => _openGallery(index, _allAlbumsData),
+                  child: AlbumsCard(albumsData: _allAlbumsData[index]),
+                );
+              },
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  /// Открытие виджета показа изображений
+  void _openGallery(index, List<AlbumsModel> _allImages) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => ImageGalleryWidget(
+          images: _allImages,
+          index: index,
+        ),
+      ),
+    );
+  }
+}
